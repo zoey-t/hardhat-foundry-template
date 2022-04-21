39e394e314c9c498d0b2e1e70bd83a1861eb9d23
ada173 <101599447+zoey-t@users.noreply.github.com>
2022-04-20 18:35:24 -0700

    add dstest plus, forge-std, hardhat console

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hardhat Foundry Template
+This repo use the default file structure of hardhat.
+On top of this, foundry support is added.
+To set up foundry support on a hardhat project:
+    1. 
---
+++ contracts/test/Greeter.t.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import ".//utils/DSTestPlus.sol";
+
+contract GreeterTest is DSTestPlus {
+
+    function setUp() public {
+        
+    }
+
+    function testTrue() public {
+        assertFalse(false);
+    }
+}
